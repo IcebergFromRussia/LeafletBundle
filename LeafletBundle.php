155fc330b1ecc7d44e913edddc7326cdff650fec
Albert <adalbert.iceberg@gmail.com>
2019-01-23 10:02:39 +0300

    init bundle

---
+++ LeafletBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Альберт
+ * Date: 23.01.2019
+ * Time: 7:42
+ */
+
+namespace LeafletBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LeafletBundle extends Bundle
+{
+
+}
